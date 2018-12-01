3ea6d34543635ff3b0500221eb6dba1f692b827a
matthewdowney18 <matthew_downey@student.uml.edu>
2018-12-01 11:22:09 -0500

    creates embedding matrix for training

---
+++ filter_embeddings.py
@@ -0,0 +1,47 @@
+from vecto import embeddings
+import numpy as np
+import pickle
+
+from dataset import SentenceDataset
+
+
+def get_vectors(filename):
+    objects = dict()
+    with (open(filename, "rb")) as openfile:
+        while True:
+            try:
+                objects = pickle.load(openfile)
+            except EOFError:
+                break
+    return objects
+
+
+def main():
+    max_len = 20
+    min_count = 2
+
+    embeddings_dir = '/home/mattd/embeddings/reddit/'
+
+    dataset_path = '/home/mattd/datasets/AskReddit/'
+    dataset_train_filename = "{}train.csv".format(dataset_path)
+    dataset_val_filename = "{}validation.csv".format(dataset_path)
+
+    dataset_train = SentenceDataset(dataset_train_filename, max_len, min_count)
+    dataset_val = SentenceDataset(dataset_val_filename, max_len, min_count,
+                                  dataset_train.vocab)
+    #dataset.add_file(eng_fr_filename2)
+
+    vectors = embeddings.load_from_dir(embeddings_dir)
+
+    #emb = embeddings.load_from_dir(embeddings_dir)
+
+    embs_matrix = np.zeros((len(dataset_val.vocab), len(vectors.matrix[0])))
+
+    for i, token in enumerate(dataset_val.vocab.token2id):
+        if vectors.has_word(token):
+            embs_matrix[i] = vectors.get_vector(token)
+    np.save('embeddings_min{}_max{}'.format(min_count, max_len), embs_matrix)
+
+
+if __name__ == '__main__':
+    main()
